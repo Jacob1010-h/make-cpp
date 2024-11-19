352a7ba0cc587ece350a93431faed11ca110eae5
Jacob1010-h <lordgaming1010@gmail.com>
2024-11-19 16:51:35 -0700

    modify code for better stuff (SFML)

--- make-cpp.bat
+++ make-cpp.bat
@@ -4,6 +4,8 @@ setlocal
 REM Define project name and file paths
 set "MAKEFILE=Makefile"
 set "MAIN_CPP=main.cpp"
+set "SFML_MAKEFILE_TEMPLATE=C:\Windows\System32\SFMLMakefileTemplate.txt"
+set "MAKEFILE_TEMPLATE=C:\Windows\System32\MakefileTemplate.txt"
 
 :menu
 cls
@@ -25,10 +27,9 @@ echo.
 echo ========================================
 echo.
 set /p choice=Choose an option (1-6) ^| exit: (6, e, q): 
-
 if "%choice%"=="1" goto compile_project
 if "%choice%"=="2" goto clean_project
-if "%choice%"=="3" goto create_makefile
+if "%choice%"=="3" goto makefile
 if "%choice%"=="4" goto init_project
 if "%choice%"=="5" goto zip_project
 if /I "%choice%"=="6" goto exit_program
@@ -36,41 +37,70 @@ if /I "%choice%"=="e" goto exit_program
 if /I "%choice%"=="E" goto exit_program
 if /I "%choice%"=="q" goto exit_program
 if /I "%choice%"=="Q" goto exit_program
-
-echo Invalid choice. Please enter a number between 1 and 5, or e/E/q/Q to exit.
+echo Invalid choice. Please enter a number between 1 and 6, or e/E/q/Q to exit.
 pause
 goto menu
 
+:check_make
+where make >nul 2>nul
+if errorlevel 1 (
+  echo 'make' is not recognized as an internal or external command, operable program or batch file.
+  echo Please install 'make' and ensure it is added to your PATH.
+  pause
+  goto menu
+)
+goto %1
+
 :compile_project
+call :check_make compile_project_continue
+:compile_project_continue
 echo.
 echo Compiling project...
 make
 if errorlevel 1 (
-    echo Compilation failed.
+  echo Compilation failed.
 ) else (
-    echo Compilation successful.
+  echo Compilation successful.
 )
 pause
 goto menu
 
 :clean_project
+call :check_make clean_project_continue
+:clean_project_continue
 echo.
 echo Cleaning project...
 make clean
 pause
 goto menu
 
-:create_makefile
-echo.
-echo Creating new Makefile...
-type "C:\Windows\System32\MakefileTemplate.txt" > %MAKEFILE%
-echo Makefile created successfully.
+:makefile
+set /p sfml="Is this an SFML project? (y/n): "
+if /i "%sfml%"=="y" (
+  type "%SFML_MAKEFILE_TEMPLATE%" > %MAKEFILE%
+) else (
+  type "%MAKEFILE_TEMPLATE%" > %MAKEFILE%
+)
+echo Makefile created using %MAKEFILE%.
 pause
 goto menu
 
 :init_project
 echo.
+set /p confirm="Are you sure you want to initialize a new C++ project? (y/n): "
+if /i "%confirm%" NEQ "y" (
+  echo Project initialization canceled.
+  pause
+  goto menu
+)
+
 echo Initializing new C++ project...
+set /p sfml="Is this an SFML project? (y/n): "
+if /i "%sfml%"=="y" (
+  type "%SFML_MAKEFILE_TEMPLATE%" > %MAKEFILE%
+) else (
+  type "%MAKEFILE_TEMPLATE%" > %MAKEFILE%
+)
 (
 echo #include ^<iostream^> > %MAIN_CPP%
 echo using namespace std; >> %MAIN_CPP%
@@ -80,41 +110,36 @@ echo     return 0; >> %MAIN_CPP%
 echo } >> %MAIN_CPP%
 )
 echo New C++ project initialized with %MAIN_CPP%.
-goto create_makefile
-
-REM zip project to ../Zip/[name of project] if the folder exists, if not, crreate it and then zip
+pause
+goto menu
 
 :zip_project
-
+call :check_make zip_project_continue
+:zip_project_continue
 for %%I in (.) do set folder=%%~nI%%~xI
-
 REM make sure that the current directory contains a makefile file
-
 if exist %MAKEFILE% (
-    echo.
-    echo Compiling project...
-    make
+  echo.
+  echo Compiling project...
+  make
 ) else (
-    echo Makefile not found in current directory. Exiting...
-    pause
-    goto menu
+  echo Makefile not found in current directory. Exiting...
+  pause
+  goto menu
 )
-
 echo.
 echo Cleaning project...
 make clean
-
 if exist ..\Zip (
-    echo Zipping project to..\Zip\...
-    tar -a -c -f ../Zip/%folder%.zip *.*
-    echo Project zipped successfully.
+  echo Zipping project to..\Zip\...
+  tar -a -c -f ../Zip/%folder%.zip *.*
+  echo Project zipped successfully.
 ) else (
-    mkdir..\Zip
-    echo Zipping project to..\Zip\...
-    tar -a -c -f ../Zip/%folder%.zip *.*
-    echo Project zipped successfully.
+  mkdir ..\Zip
+  echo Zipping project to..\Zip\...
+  tar -a -c -f ../Zip/%folder%.zip *.*
+  echo Project zipped successfully.
 )
-
 echo.
 pause
 goto menu
